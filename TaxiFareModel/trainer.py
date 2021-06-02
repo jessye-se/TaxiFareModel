8504da5d5d710a6a509ec93eef161a973783b040
Jessye Gabella <jessye.seleucide@gmail.com>
2021-06-02 10:53:36 +0200

    all changes to be commit from TaxiFareModel

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -3,8 +3,13 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
+from memoized_property import memoized_property
+
 
 class Trainer():
+    MLFLOW_URI = "https://mlflow.lewagon.co/"
+    EXPERIMENT_NAME = "[FR] [Marseille] [Sidjee-se] model1 + 1.0"  
+
     def __init__(self, X, y):
         """
             X: pandas DataFrame
@@ -48,6 +53,27 @@ class Trainer():
         rmse = compute_rmse(y_pred, y_test)
         print(rmse)
         return rmse
+
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 if __name__ == "__main__":
     # get data
